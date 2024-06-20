4b1990788ec020b188af0e9b68ca7c21b182c500
unknown <tomfraczekdev@gmail.com>
2024-06-20 13:59:42 +0200

    feat: add jwt check for the middleware

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -6,15 +6,19 @@ const createCurrentUser = async (req: Request, res: Response) => {
     const { auth0Id } = req.body;
     const existingUser = await User.findOne({ auth0Id });
 
-    if (existingUser) res.status(200).send();
+    if (existingUser) {
+      return res.status(200).send();
+    }
 
     const newUser = new User(req.body);
     await newUser.save();
 
-    res.status(201).json(newUser.toObject());
+    return res.status(201).json(newUser.toObject());
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Error creating user" });
+    if (!res.headersSent) {
+      return res.status(500).json({ message: "Error creating user" });
+    }
   }
 };
 
